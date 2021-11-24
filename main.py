74c248951a5db901e3e4ae02afcd0066339f7ef1
Mateo Marcelic <marcelicmateo@protonmail.com>
2021-11-24 21:23:20 +0100

    common callback

--- main.py
+++ main.py
@@ -23,7 +23,11 @@ cx = []
 for key, data in data.items():
     cx.append(
         hw_collection(
-            button=Button(key),
+            button=Button(
+                key,
+                bounce_time=0.030,
+                pull_up=False,
+            ),
             audio=Path(data.get("audio")),
             led=PWMLED(data.get("led_pin")),
         )
@@ -36,48 +40,34 @@ player = MPV(vid="no", input_vo_keyboard=False)
 FADE_TIME = 1
 
 
-def cb_b1():
+def cb_b(number):
     player.stop()
-    player.play(cx[0].audio)
+    player.play(cx[number].audio)
     for i, c in enumerate(cx):
-        if i == 0:
+        if i == number:
             c.led.pulse(fade_in_time=FADE_TIME, fade_out_time=0, n=1)
         elif c.led.value != 0:
             c.led.pulse(fade_in_time=0, fade_out_time=FADE_TIME, n=1)
+
+
+def cb_b1():
+    cb_b(1)
 
 
 def cb_b2():
-    player.stop()
-    player.play(cx[0].audio)
-    for i, c in enumerate(cx):
-        if i == 1:
-            c.led.pulse(fade_in_time=FADE_TIME, fade_out_time=0, n=1)
-        elif c.led.value != 0:
-            c.led.pulse(fade_in_time=0, fade_out_time=FADE_TIME, n=1)
+    cb_b(2)
 
 
 def cb_b3():
-    player.stop()
-    player.play(cx[0].audio)
-    for i, c in enumerate(cx):
-        if i == 2:
-            c.led.pulse(fade_in_time=FADE_TIME, fade_out_time=0, n=1)
-        elif c.led.value != 0:
-            c.led.pulse(fade_in_time=0, fade_out_time=FADE_TIME, n=1)
+    cb_b(3)
 
 
 def cb_b4():
-    player.stop()
-    player.play(cx[0].audio)
-    for i, c in enumerate(cx):
-        if i == 3:
-            c.led.pulse(fade_in_time=FADE_TIME, fade_out_time=0, n=1)
-        elif c.led.value != 0:
-            c.led.pulse(fade_in_time=0, fade_out_time=FADE_TIME, n=1)
+    cb_b(4)
 
 
 for i, c in enumerate(cx):
-    c.button = eval("cb_b{}".format(i))
+    c.button.when_pressed = eval("cb_b{}".format(i))
 
 
 from time import sleep
